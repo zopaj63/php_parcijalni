754a0f428fd597a41db1fd0ef433304fb1389cd7
zopaj63 <zoran.pajnic@gmail.com>
2023-05-26 19:35:03 +0200

    postavljanje

--- tablica.php
+++ tablica.php
@@ -1,1 +1,26 @@
-<p>tablica</p>
+<?php
+    $popis=file_get_contents("words.json");
+    $rijeci=json_decode($popis, true);  // json pretvoren u niz, sa true čita cijeli asocijativni niz
+?>
+
+<table border="1">
+    <tr>
+        <th>Riječ</th>
+        <th>Broj slova</th>
+        <th>Broj suglasnika</th>
+        <th>Broj samoglasnika</th>
+    </tr>
+    
+    <?php
+        foreach ($rijeci as $rijec)
+        {
+            echo "<tr>";
+                echo "<td>".$rijec["word"]."</td>";
+                echo "<td>".$rijec["word"]."</td>";
+                echo "<td>".$rijec["word"]."</td>";
+                echo "<td>".$rijec["word"]."</td>";
+            echo "</tr>";
+        };
+    ?>
+
+</table>
--- wordin.php
+++ wordin.php
@@ -1,13 +1,30 @@
 <?php
     // podaci s forme u php-ispit.php
+
+    $niz=[];
+
     if($_POST)
     {
         $word=$_POST["word"];
+        //provjera da li je polje prazno
+        if(!empty($word))
+        {
+            $cleanword=trim($word); //trim briše prazne znakove prije početka i iza kraja
+            //kreiranje json datoteke
+            $jsonWord=json_encode($cleanword);
+            $niz=[$jsonWord];
+            $datoteka="words.json";
+            file_put_contents($datoteka, $niz, FILE_APPEND); //generira json fidatoteku
+        }
+        else
+        {
+            echo "Polje ne smije biti prazno!";
+        }
     }
     else
     {
         echo "Nisam pročitao riječ?!";
     }
 
-echo $word;
+//echo $word;
 ?>
